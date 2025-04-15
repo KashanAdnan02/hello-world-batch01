fd1fa7dee7d47d8ead9b0227ad8e97856c903283
Almiraadnan <almiraadnan34@gmail.com>
2025-04-15 19:26:33 +0500

    some-code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // let count = 0
+  let [value, setValue] = useState("")
+  // let [value2, setValue2] = useState("")
+  console.log(value)
+
+
+  // if (value.indexOf("@") === -1){
+  //   console.log("It is not a email");
+    
+  // }
+    // function increment() {
+    //   setCount(++count)
+    // }
+    // console.log(count)
+
+    // function decrement() {
+    //   setCount(--count)
+    // }
+
+
+
+
+    return (
+      <div style={{ display: "flex", alignItems: "center", gap: "20px" }}>
+        <input type='text' onChange={(e) => setValue(e.target.value)} />
+
+        {/* <button onClick={decrement}>-</button>
+      <h1>{count}</h1>
+      <button onClick={increment}>+</button> */}
+      </div>
+    );
 }
 
 export default App;
